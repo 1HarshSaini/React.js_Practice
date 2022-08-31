1fd9e124ec9cfb86874890c154343e4aa90bf23e
Harsh Saini <harshsaini100200@gmail.com>
2022-08-31 19:18:56 +0530

    usestete

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Student'
 import Student from './Student'
 function App() {
+  const [name,setName]=useState("Harsh");
   return (
     <>
-    <Student name={"Harsh Saini"} email={"harshsaaii100200"} other={{address:'Delhi',occupation:'student'}}/>
-    <Student name={"Vishu Saini"} email={"harsh100200"} other={{address:'Delhi',occupation:'student'}}/>
+    {/* <Student name={"Harsh Saini"} email={"harshsaaii100200"} other={{address:'Delhi',occupation:'student'}}/>
+    <Student name={"Vishu Saini"} email={"harsh100200"} other={{address:'Delhi',occupation:'student'}}/> */}
+
+    <Student name={name}/>
+    <button onClick={()=>setName("SuryaVanshi")}>Update Name</button>
+
     </>
   )
 }
--- react-app/src/Student.js
+++ react-app/src/Student.js
@@ -3,8 +3,8 @@ function Student(props){
         <>
         {/* <h1>This is componnet1</h1> */}
         <h2>Hello {props.name} </h2>
-        <p>EMail {props.email} </p>
-        <h3>{props.other.address}</h3>
+        {/* <p>EMail {props.email} </p>
+        <h3>{props.other.address}</h3> */}
         
         </>
     )
