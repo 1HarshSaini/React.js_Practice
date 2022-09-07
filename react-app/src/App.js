e0d80eab99523c957e3bfe4cf722dae8cd8a5af2
Harsh Saini <harshsaini100200@gmail.com>
2022-09-07 15:00:24 +0530

    form

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,24 @@
-import React,{useState} from 'react'
-import './index.css'
-function App() {
-  const [status,setStatus]=useState(false);
-  return (
-    <div className='App'>
-        {
-          status?<h1>Showing on chenging state</h1>:null
-        }
-        {/* <button onClick={()=>setStatus(false)}>Hide</button>
-        <button onClick={()=>setStatus(true)}>show</button> */}
+import React from 'react'
 
-        <button onClick={()=>setStatus(!status)}>Toggle</button>
+function App() {
+  function getFormData(event){
+      event.preventDefault();
+  }
+  return (
+    <div>
+      <h2>Form in React</h2>
+      <form onSubmit={getFormData}>
+        <input type="text" placeholder='Enter Name'/><br />
+        <select>
+          <option value="">DSA</option>
+          <option value="">F&D</option>
+          <option value="">NALR</option>
+        </select><br /><br />
+        <input type="checkbox" /><span>Tick </span>
+        <br /><br />
+        <button type="submit">Submit</button>
+
+      </form>
     </div>
   )
 }
